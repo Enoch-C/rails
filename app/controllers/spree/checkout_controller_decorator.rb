8de4757be1ac82ab0c6998c0f680a77668ca477e
Tian <tian@snapfit.com>
2016-05-16 17:11:31 +0800

    modify the checkout registration path

---
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -0,0 +1,10 @@
+Spree::CheckoutController.class_eval do
+  private
+    # Introduces a registration step whenever the +registration_step+ preference is true.
+    def check_registration
+      return unless Spree::Auth::Config[:registration_step]
+      return if spree_current_user or current_order.email
+      store_location
+      redirect_to spree.login_path
+    end
+end
