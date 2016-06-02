761bef746638695582e038757e5b9f20fc05bcdf
Tian <tian@snapfit.com>
2016-06-02 15:35:11 +0800

    fix the bug

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,14 +10,12 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
-  # Example:
-  # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
+  config.admin_interface_logo = 'logo/logo.png'
+  config.logo = 'logo/logo.png'
 end
 
 Spree.user_class = "Spree::User"
-config.admin_interface_logo = 'logo/logo.png'
-config.logo = 'logo/logo.png'
+
 config = Rails.application.config
 config.after_initialize do
   config.spree.calculators.shipping_methods << Spree::Calculator::Shipping::PerTwoItems
