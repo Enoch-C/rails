c41755b26dff5824056dc52f14ac8319220094de
Tian <tian@snapfit.com>
2016-06-03 15:52:41 +0800

    change to up to three selections

--- db/samples/ccstock.rb
+++ db/samples/ccstock.rb
@@ -1,7 +1,7 @@
 Spree::Sample.load_sample("ccvariants")
 
 country =  Spree::Country.find_by(iso: 'US')
-state =  Spree::Country::State.find_by_name('California')
+state =  Spree::State.find_by_name('California')
 location = Spree::StockLocation.first_or_create! name: 'Cool Choice, California, USA', address1: '1376 E. Valencia Drive', city: 'Fullerton', zipcode: '92831', country: country, state: state
 location.name = 'Cool Choice, California, USA'
 location.address1 = '1376 E. Valencia Drive'
--- db/samples/ccvariants.rb
+++ db/samples/ccvariants.rb
@@ -8,19 +8,20 @@ func3_id = Spree::OptionType.find_by_name!("func3").id
 tax_category = Spree::TaxCategory.find_by_name!("USA")
 
 Rails.application.config.product_list.each_with_index do |product_name, product_index|
+
   Rails.application.config.function_list.combination(3).each_with_index do |funcs, index|
     cost_price = 0
     sku = ""
     case product_index
     when 0
       cost_price = 3.99
-      sku = "ccsd"
+      sku = "ccsd3f"
     when 1
       cost_price = 13.99
-      sku = "ccsw"
+      sku = "ccsw3f"
     when 2
       cost_price = 39.99
-      sku = "ccsm"
+      sku = "ccsm3f"
     end
     sku = sku + format('%04d', index + 1)
     unless Spree::Variant.find_by(sku: sku)
@@ -34,6 +35,61 @@ Rails.application.config.product_list.each_with_index do |product_name, product_
       })
     end
   end
+
+  Rails.application.config.function_list.combination(2).each_with_index do |funcs, index|
+    cost_price = 0
+    sku = ""
+    case product_index
+    when 0
+      cost_price = 3.99
+      sku = "ccsd2f"
+    when 1
+      cost_price = 13.99
+      sku = "ccsw2f"
+    when 2
+      cost_price = 39.99
+      sku = "ccsm2f"
+    end
+    sku = sku + format('%04d', index + 1)
+    unless Spree::Variant.find_by(sku: sku)
+      Spree::Variant.create!(
+      {
+        :product => Spree::Product.find_by_name!(product_name),
+        :option_values => [Spree::OptionValue.find_by_name_and_option_type_id!(funcs[0], func1_id), Spree::OptionValue.find_by_name_and_option_type_id!(funcs[1], func2_id)],
+        :sku => sku,
+        :cost_price => cost_price,
+        :tax_category => tax_category
+      })
+    end
+  end
+
+  Rails.application.config.function_list.combination(1).each_with_index do |funcs, index|
+    cost_price = 0
+    sku = ""
+    case product_index
+    when 0
+      cost_price = 3.99
+      sku = "ccsd1f"
+    when 1
+      cost_price = 13.99
+      sku = "ccsw1f"
+    when 2
+      cost_price = 39.99
+      sku = "ccsm1f"
+    end
+    sku = sku + format('%04d', index + 1)
+    unless Spree::Variant.find_by(sku: sku)
+      Spree::Variant.create!(
+      {
+        :product => Spree::Product.find_by_name!(product_name),
+        :option_values => [Spree::OptionValue.find_by_name_and_option_type_id!(funcs[0], func1_id)],
+        :sku => sku,
+        :cost_price => cost_price,
+        :tax_category => tax_category
+      })
+    end
+  end
+
 end
 
 # masters.each do |product, variant_attrs|
