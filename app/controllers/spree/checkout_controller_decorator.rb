936e394a8c5f9ba5dec74747f3e6b3049a224b50
Tian <tian@snapfit.com>
2016-06-17 03:44:27 +0800

    add stripe checkout with alipay

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,9 +2,15 @@ module Spree
   Spree::CheckoutController.class_eval do
     before_action :ensure_stripe_checkout, only: [:update]
 
+    private
+
     def ensure_stripe_checkout
+      puts params
       if params[:stripeToken]
-        stripe = PaymentMethod.find_by_name("Stripe")
+        stripe = PaymentMethod.find_by_type("Spree::Gateway::StripeGateway")
+        unless params[:payment_source]
+          params[:payment_source] = {}
+        end
         params[:payment_source][stripe.id.to_s] = { :gateway_payment_profile_id => params[:stripeToken],
           :stripeTokenType => params[:stripeTokenType],
           :stripeEmail => params[:stripeEmail] }
@@ -12,6 +18,8 @@ module Spree
         params.delete(:stripeTokenType)
         params.delete(:stripeEmail)
       end
+      puts params
+
     end
   end
 end
