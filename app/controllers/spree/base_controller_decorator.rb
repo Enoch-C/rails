a46adf3fed09fb0999fcfe58c96b3cc96758ab30
Tian <tian@snapfit.com>
2016-06-28 19:45:06 +0800

    zh-CN wordings

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -5,11 +5,17 @@ module Spree
     private
       def set_locale
         return if cookies["cc-locale"]
-        puts I18n.available_locales
-        puts http_accept_language.preferred_language_from(I18n.available_locales)
-        I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+        accept_langs = env["HTTP_ACCEPT_LANGUAGE"]
+        return if accept_langs.nil?
+        languages_and_qvalues = accept_langs.split(",").map { |l|
+          l += ';q=1.0' unless l =~ /;q=\d+(?:\.\d+)?$/
+          l.split(';q=')
+        }
+        lang = languages_and_qvalues.sort_by { |(locale, qvalue)|
+          qvalue.to_f
+        }.last.first
+        lang == '*' ? I18n.locale = 'en-US' : I18n.locale = lang
         puts "* Locale set to '#{I18n.locale}'"
-
       end
 
   end
