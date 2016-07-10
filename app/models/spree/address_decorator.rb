8c89722f8be376a82d84a655c03429f3622e9658
Tian <tian@snapfit.com>
2016-07-09 20:56:38 -0700

    ONLY us and ca using state abbr.

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,7 +1,11 @@
 module Spree
   Spree::Address.class_eval do
     def state_text
-      state.try(:name) || state_name
+      if this.country.name.start_with?("United States") || this.country.name.start_with?("Canada")
+        state.try(:abbr) || state.try(:name) || state_name
+      else
+        state.try(:name) || state_name
+      end
     end
   end
 end
