15ac03683a121aca9c9a23504d1e71e6f518f4c8
Tian <tian@snapfit.com>
2016-07-10 23:18:50 -0700

    rm develop email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            'mailer@coolchoice.com',
-    password:             'ntebmhqypyygyoll',
+    user_name:            'rmmailer@coolchoice.com',
+    password:             'rmntebmhqypyygyoll',
     authentication:       'plain',
     enable_starttls_auto: true
     }
